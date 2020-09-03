9c5e0b6621ec144c094fbda46064ca3267b7c903
AltCtrlDel1999 <shubhamrampurkar3@gmail.com>
2020-09-03 19:25:28 +0530

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './component/Header';
+//import 'bootstrap/dist/bootstrap.min.css';
 import Home from './component/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      {/*<Home />*/}
 
       {/*Footer*/}
     </div>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,25 +1,30 @@
 import React,{ useState } from 'react';
-import logo from '../logo.svg'
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  Media,
-  NavLink,
-} from 'reactstrap';
+import logo from '../public/logo1.png';
+
 const Header = () => {
+  const temp = {
+    float: "right"
+  }
 
   return(
-    <div>
-      <Navbar color="dark" dark expand="md">
-        <NavbarBrand href="#">MyPortFolio</NavbarBrand>
 
-
-      </Navbar>
-    </div>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
+          <a  class="navbar-brand" href="#">
+            <img src={logo} width="35" height="35" class="d-inline-block align-top" alt="" />
+            MyPortFolio
+          </a>
+          <ul class="navbar-nav justify-content-end ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Projects</a>
+            </li>
+            <li class="nav-item ml-3">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item ml-3 mr-3">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
   );
 
 }
